ab12156d16b53dcf76315c59b95525d26baecbb1
Steph Milovic <milovic@cleverbridge.com>
2015-03-22 17:07:49 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Straightup
+
+Straightup is a skeleton for quickly starting a new static site. [Gulp](http://gulpjs.com/) to concatenate and minify JS files, compress images, compile [SASS](http://sass-lang.com/), recompile assets when changes are detected, and a few other useful helpers. It also runs [BrowserSync](http://www.browsersync.io/) so you can see your changes instantly across multiple devices. Straightup utilizes [Bower](http://bower.io/) to grab front-end libraryies like jQuery. [Yeoman](http://yeoman.io/) scaffolds the application, by writing the Culp configuraltion and pulling our Gulp tasks and Bower dependencies. Straightup comes packaged with [Bourbon](http://bourbon.io/) + [Neat](http://neat.bourbon.io/) + [Bitters](http://bitters.bourbon.io/), providing you with plenty of mixins and variables/extends to give you a jump start on your site.
+
+## Installing Dependencies
+
+You need to have [Node JS](https://nodejs.org), Yeoman, Bower, and Gulp installed for the dependencies to work. Installers for Node JS can be found [here](http://nodejs.org/download/).
+
+### Installing Yeoman:
+
+```shell
+npm install -g yo
+```
+
+### Installing Bower:
+
+```shell
+npm install -g bower
+```
+
+### Installing Gulp:
+
+```shell
+npm install -g gulp
+```
+
+## Installing Project
+
+cd into your project folder and run the following commands 
+
+```shell
+bower install
+npm install
+```
+
+## WAtching files and running a dev server
+
+```shell
+gulp serve
+```
+
+This will run a server for the project that can be accessed at [http://localhost:9000](http://localhost:9000). Changes to files will tell Gulp to automatically rebuild the site. Gulp will also run and compile SASS to CSS, compress images, and compile/minify JSwhen it detects changes to any of these types of files.
+
+## Building the site
+
+```shell
+gulp
+```
+
+This command will compile all assets and then build the site to the /dist directory. This directory can then be pushed to your remote site.
+
+I'm still working out changing the relative file paths. Update to come by 3/27.
---
+++ gulpfile.js
@@ -0,0 +1,145 @@
+/*global -$ */
+'use strict';
+// generated on 2015-02-24 using generator-gulp-webapp 0.3.0
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var bourbon      = require('node-bourbon').includePaths;
+var neat         = require('node-neat').includePaths;
+var sourcemaps = require('gulp-sourcemaps');
+var plumber = require('gulp-plumber');
+var concat = require('gulp-concat');
+var replace = require('gulp-replace');
+
+gulp.task('styles', function () {
+  return gulp.src('app/styles/styles.scss')
+  .pipe(plumber())
+  .pipe(sourcemaps.init())
+    .pipe($.sass({
+      style: 'expanded',
+      precision: 10,
+      includePaths: require('node-neat').includePaths,
+      onError: console.error.bind(console, 'Sass error:')
+    }))
+    .pipe($.postcss([
+     require('autoprefixer-core')({browsers: ['last 2 version']})
+    ]))
+    .pipe(sourcemaps.write())
+    .pipe(plumber.stop())
+    .pipe(gulp.dest('.tmp/styles'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('jshint', function () {
+  return gulp.src('app/scripts/**/*.js')
+    .pipe(reload({stream: true, once: true}))
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
+});
+
+// Scripts
+gulp.task('scripts', function() {
+  return gulp.src('app/scripts/*.js')
+    .pipe(plumber())
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest('.tmp/scripts'))
+});
+
+gulp.task('html', ['styles'], function () {
+  var assets = $.useref.assets({searchPath: ['.tmp', 'app', '.']});
+
+  return gulp.src('app/*.html')
+    .pipe(assets)
+    .pipe($.if('*.js', $.uglify()))
+    .pipe($.if('*.css', $.csso()))
+    .pipe(assets.restore())
+    .pipe($.useref())
+    .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', function () {
+  return gulp.src('app/images/**/*')
+    .pipe($.cache($.imagemin({
+      progressive: true,
+      interlaced: true,
+      // don't remove IDs from SVGs, they are often used
+      // as hooks for embedding and styling
+      svgoPlugins: [{cleanupIDs: false}]
+    })))
+    .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('fonts', function () {
+  return gulp.src(require('main-bower-files')({
+    filter: '**/*.{eot,svg,ttf,woff,woff2}'
+  }).concat('app/fonts/**/*'))
+    .pipe(gulp.dest('.tmp/fonts'))
+    .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('extras', function () {
+  return gulp.src([
+    'app/*.*',
+    '!app/*.html'
+  ], {
+    dot: true
+  }).pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
+
+gulp.task('serve', ['styles', 'fonts', 'scripts'], function () {
+  browserSync({
+    notify: false,
+    port: 9000,
+    server: {
+      baseDir: ['.tmp', 'app'],
+      routes: {
+        '/bower_components': 'bower_components'
+      }
+    }
+  });
+
+  // watch for changes
+  gulp.watch([
+    'app/*.html',
+    'app/scripts/*.js',
+    'app/images/**/*',
+    '.tmp/fonts/**/*'
+  ]).on('change', reload);
+
+  gulp.watch('app/styles/**/*.scss', ['styles']);
+  gulp.watch('app/fonts/**/*', ['fonts']);
+  gulp.watch('app/scripts/*.js', ['scripts']);
+  gulp.watch('bower.json', ['wiredep', 'fonts']);
+});
+
+// inject bower components
+gulp.task('wiredep', function () {
+  var wiredep = require('wiredep').stream;
+
+  gulp.src('app/styles/*.scss')
+    .pipe(wiredep({
+      ignorePath: /^(\.\.\/)+/
+    }))
+    .pipe(gulp.dest('app/styles'));
+
+  gulp.src('app/*.html')
+    .pipe(wiredep({
+      ignorePath: /^(\.\.\/)*\.\./
+    }))
+    .pipe(gulp.dest('app'));
+});
+
+gulp.task('build', ['jshint', 'html', 'images', 'fonts', 'extras'], function () {
+  return gulp.src('dist/**/*')
+  .pipe(replace('../../fonts', '../fonts'))
+  .pipe($.size({title: 'build', gzip: true}));
+});
+
+gulp.task('default', ['clean'], function () {
+  gulp.start('build');
+});
